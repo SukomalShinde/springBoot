233eeb78e9ee27f5cfd2f7c9e027bde11aa4498a
SukomalShinde <Sukomalshinde@gmail.com>
2022-05-05 19:00:23 -1200

    first commit

---
+++ src/main/java/com/demo/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	
+	
+	
+	
+}
---
+++ src/main/java/com/demo/service/EmployeeService.java
@@ -0,0 +1,29 @@
+package com.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.entity.Employee;
+import com.demo.repository.EmployeeRepository;
+@Service
+public class EmployeeService {
+	@Autowired
+	private EmployeeRepository repo;
+	
+	public Employee saveEmployee(Employee emp) {
+		
+		return repo.save(emp);
+	}
+	
+	
+	public List<Employee> getAllEmployees(){
+		
+		return repo.findAll();
+	}
+	
+
+	
+	
+}
